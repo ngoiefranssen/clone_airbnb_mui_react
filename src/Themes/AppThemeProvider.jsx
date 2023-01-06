c97a6aa87d7ecda4ab02eda19baf681badd7abc3
Franssen <ngoiefranssen04@gmail.com>
2023-01-06 15:18:12 +0200

    Wip

--- src/Themes/AppThemeProvider.jsx
+++ src/Themes/AppThemeProvider.jsx
@@ -1,7 +1,20 @@
 import { createTheme, ThemeProvider } from '@mui/material';
 import React from 'react'
 
-const theme = createTheme({});
+const theme = createTheme({
+    typography:{
+        allVariants:{
+            fontFamily: 'Raleway',
+            textTransform: 'none',
+            fontSize: 15
+        },
+    },
+    palette:{
+        primary:{
+            
+        }
+    }
+});
 
 const AppThemeProvider = (prop) => {
     return (
