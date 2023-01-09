5ec90fff157e6bc4089beb17eb8eb88ed9ad1346
Franssen <ngoiefranssen04@gmail.com>
2023-01-09 11:28:41 +0200

    Wip

--- src/Components/OptionsTab.jsx
+++ src/Components/OptionsTab.jsx
@@ -1,5 +1,6 @@
-import { Container } from '@mui/material';
+import { Box, Container, Tab, Tabs, tabsClasses } from '@mui/material';
 import React from 'react'
+import { locationsTab } from './MockData';
 
 const OptionsTab = () => {
     const [value, setValue] = React.useState(0)
@@ -7,8 +8,34 @@ const OptionsTab = () => {
         setValue(newEvent);
     }
     return (
-        <Container>
-
+        <Container maxWidth="xl">
+            <Box
+            sx={{
+                display: 'flex',
+                flexGrow: 1,
+                px: { xs: 0, md: 2 },
+                alignItems: "center",
+                mt: 2,
+                mb: 2,
+            }}>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    variant='scrollable'
+                    scrollButtons
+                    sx={{
+                        [`& .${tabsClasses.scrollButtons}`] : {
+                            '&.Mui-disabled' : { opacity: 0.3 }
+                        }
+                    }}
+                    >
+                        {
+                            locationsTab.map((tab) => {
+                                return <Tab key={tab.id} icon={tab.com} />
+                            })
+                        }
+                    </Tabs>
+            </Box>
         </Container>
     )
 }
