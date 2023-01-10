f6c948dd50a773b0f7bc826d1ad42f79621a6a09
Franssen <ngoiefranssen04@gmail.com>
2023-01-10 09:31:58 +0200

    Wip

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,6 @@ import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
-// react icons
 import { BsGlobe } from 'react-icons/bs';
 import { IoChevronUpOutline } from 'react-icons/io5';
 
